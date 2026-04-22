9876a8b1c52302a8517616e093cd5eced0530556
HeonJe LEE <lhjnano@gmail.com>
2026-04-23 00:00:00 +0900

    fix: installation with npm
    Signed-off-by: HeonJe LEE <lhjnano@gmail.com>

--- hits_core/storage/__init__.py
+++ hits_core/storage/__init__.py
@@ -1,11 +1,16 @@
 """Storage package with Redis and file backends."""
 
 from .base import BaseStorage
-from .redis_store import RedisStorage
 from .file_store import FileStorage
+
+try:
+    from .redis_store import RedisStorage
+except ImportError:
+    RedisStorage = None  # redis not installed, optional
 
 __all__ = [
     "BaseStorage",
-    "RedisStorage",
     "FileStorage",
 ]
+if RedisStorage is not None:
+    __all__.append("RedisStorage")
